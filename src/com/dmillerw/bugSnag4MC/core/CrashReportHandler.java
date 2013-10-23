0b69996715984c18421266096d80ba32a6df6908
dmillerw <dmillerw@gmail.com>
2013-10-22 22:21:26 -0700

    Revert to prove transformer "works"

--- src/com/dmillerw/bugSnag4MC/core/CrashReportHandler.java
+++ src/com/dmillerw/bugSnag4MC/core/CrashReportHandler.java
@@ -2,8 +2,10 @@ package com.dmillerw.bugSnag4MC.core;
 
 public class CrashReportHandler {
 
-	public static void handleCrashReport(String description, Throwable throwable, String completeReport) {
-		
+	public static void test() {
+		for (int i=0; i<20; i++) {
+			System.out.println("WORKING!");
+		}
 	}
 	
 }
